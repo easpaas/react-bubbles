ad6053ee48d06d8e9bc6b188711d52fab7a8ce31
evan <aspaasevan@gmail.com>
2020-04-24 08:35:06 -0700

    axios get colors successfully renders

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -7,6 +7,24 @@ import ColorList from "./ColorList";
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+      
+    axios.get('http://localhost:5000/api/colors', {
+      headers: {
+        Authorization: token
+      }
+    })
+    .then(response => {
+      setColorList(response.data);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, []);
+
+
   // set that data to the colorList state property
 
   return (
